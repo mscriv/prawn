0d1ceca08e191813438003762f09817b5027165a
Michael Scriven <bigscriv03@gmail.com>
2015-03-12 19:50:43 -0400

    Added full price helper to lineitems

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -3,5 +3,15 @@ class OrdersController < ApplicationController
   end
 
   def show
+    @order = Order.find(params[:id])
+    respond_to do |format|
+      format.html
+      format.pdf do
+        pdf = Prawn::Document.new
+        pdf.text 'Hello World'
+        send_data pdf.render
+      end
+
+    end
   end
 end
--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -1,3 +1,6 @@
 class LineItem < ActiveRecord::Base
   belongs_to :order
+  def full_price
+    unit_price * quantity
+  end
 end
